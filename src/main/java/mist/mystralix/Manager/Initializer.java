ceab9d155aad3c708a2d196daeefcf2411822b2b
astevenmics <aljohnstevenmiclat.kes@gmail.com>
2025-11-08 00:34:30 -0600

    === Clock Work | 0.0.0.0.2.4.5 ===
    - Main.java
    * Improved formatting
    
    - FileHandler.java
    * Updated writeJSONInitializerInFile by adding String initializer parameter to determine what initializer to use per file as HashMap uses [] and HashSet uses {}
    * Refactored code
    
    - Initializer.java
    * Refactored code
    * Removed unused import statements

--- src/main/java/mist/mystralix/Manager/Initializer.java
+++ src/main/java/mist/mystralix/Manager/Initializer.java
@@ -3,7 +3,6 @@ package mist.mystralix.Manager;
 import mist.mystralix.ExternalFileHandler.FileHandler;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.util.Objects;
 
 public class Initializer {
