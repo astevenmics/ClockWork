9d8ca1a43a4bf9c4badbdb2fd5a88ca6d2067433
softmist_1 <aljohnstevenmiclat.kes@gmail.com>
2025-12-07 18:11:30 -0600

    === Clock Work | 0.0.0.0.2.18.48 ===
    - TeamSubCommandFunctions.java
    * Dependency Injection for TeamService
    * Initiated TeamEmbed upon calling TeamSubCommandFunctions
    * Initiated create functionality

--- src/main/java/mist/mystralix/presentation/commands/slash/team/TeamSubCommandFunctions.java
+++ src/main/java/mist/mystralix/presentation/commands/slash/team/TeamSubCommandFunctions.java
@@ -1,13 +1,42 @@
 package mist.mystralix.presentation.commands.slash.team;
 
+import mist.mystralix.application.team.TeamService;
+import mist.mystralix.domain.team.Team;
 import mist.mystralix.presentation.commands.slash.ISlashCommandCRUD;
+import mist.mystralix.presentation.embeds.TeamEmbed;
 import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 
+import java.util.ArrayList;
+
 public class TeamSubCommandFunctions implements ISlashCommandCRUD {
+
+    private final TeamService TEAM_SERVICE;
+    private final TeamEmbed TEAM_EMBED = new TeamEmbed();
+
+    public TeamSubCommandFunctions(TeamService teamService) {
+        this.TEAM_SERVICE = teamService;
+    }
+
     @Override
     public MessageEmbed create(SlashCommandInteraction event) {
-        return null;
+        OptionMapping optionName = event.getOption("team_name");
+        if (optionName == null) {
+            // TODO: Update return embed for errors
+            return null;
+        }
+
+        User user = event.getUser();
+
+        String name = optionName.getAsString();
+        ArrayList<String> moderators = new ArrayList<>();
+        moderators.add(user.getId());
+
+        Team team = TEAM_SERVICE.create(name, moderators);
+
+        return TEAM_EMBED.createMessageEmbed(user, "New Team", team);
     }
 
     @Override
