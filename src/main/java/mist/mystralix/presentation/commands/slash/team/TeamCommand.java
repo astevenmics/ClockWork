91f000561e3badd6aefa318249df8973fdacaf83
softmist_1 <aljohnstevenmiclat.KES@gmail.com>
2025-12-07 18:07:35 -0600

    === Clock Work | 0.0.0.0.2.18.45 ===
    - TeamCommand.java
    * Added SubCommand data
    * Added SubCommandGroup data
    * Implemented new method from SlashCommand interface
    * Initiated execute method functionality

--- src/main/java/mist/mystralix/presentation/commands/slash/team/TeamCommand.java
+++ src/main/java/mist/mystralix/presentation/commands/slash/team/TeamCommand.java
@@ -1,9 +1,15 @@
 package mist.mystralix.presentation.commands.slash.team;
 
 import mist.mystralix.application.team.TeamService;
+import mist.mystralix.domain.enums.TaskStatus;
 import mist.mystralix.presentation.commands.slash.SlashCommand;
+import mist.mystralix.presentation.commands.slash.task.TaskSubCommandFunctions;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData;
 
 public class TeamCommand implements SlashCommand {
 
@@ -26,11 +32,217 @@ public class TeamCommand implements SlashCommand {
 
     @Override
     public SubcommandData[] getSubcommands() {
-        return new SubcommandData[]{ };
+        return new SubcommandData[]{
+                new SubcommandData("create", "Create a team")
+                    .addOptions(
+                        new OptionData(
+                            OptionType.STRING,
+                            "team_name",
+                            "Team name",
+                            true
+                        ).setRequiredLength(1, 32)
+                ),
+                new SubcommandData("add", "Add a user in a team")
+                    .addOptions(
+                        new OptionData(
+                            OptionType.INTEGER,
+                            "id",
+                            "Team ID of the team you want to modify",
+                            true
+                        ),
+                        new OptionData(
+                            OptionType.USER,
+                            "user",
+                            "Mention a user to add to team",
+                            true
+                        )
+                ),
+                new SubcommandData("remove", "Removes a user in a team")
+                    .addOptions(
+                        new OptionData(
+                            OptionType.INTEGER,
+                            "id",
+                            "Team ID of the team you want to modify",
+                            true
+                        ),
+                        new OptionData(
+                            OptionType.USER,
+                            "user",
+                            "Mention a user to remove to team",
+                            true
+                        )
+                ),
+                new SubcommandData("accept", "Accept a team invitation using team ID")
+                    .addOptions(
+                        new OptionData(
+                            OptionType.INTEGER,
+                            "id",
+                            "Team ID of the team you want to accept",
+                            true
+                        )
+                ),
+                new SubcommandData("reject", "Reject a team invitation using team ID")
+                    .addOptions(
+                        new OptionData(
+                            OptionType.INTEGER,
+                            "id",
+                            "Team ID of the team you want to reject",
+                            true
+                        )
+                ),
+                new SubcommandData("leave", "Leave a team using team ID")
+                    .addOptions(
+                        new OptionData(
+                            OptionType.INTEGER,
+                            "id",
+                            "Team ID of the team you want to leave",
+                            true
+                        )
+                ),
+                new SubcommandData("view", "View a team using team ID")
+                    .addOptions(
+                        new OptionData(
+                                OptionType.INTEGER,
+                                "id",
+                                "Team ID of the team you want to view",
+                                true
+                        )
+                )
+        };
+    }
+
+    @Override
+    public SubcommandGroupData[] getSubcommandGroupData() {
+        return new SubcommandGroupData[]{
+                new SubcommandGroupData("task", "Task Management")
+                        .addSubcommands(
+                        new SubcommandData("create", "Create a task")
+                                .addOptions(
+                                        new OptionData(OptionType.INTEGER, "team", "Team ID", true),
+                                        new OptionData(OptionType.STRING, "title", "Task title", true),
+                                        new OptionData(OptionType.STRING, "description", "Task description", true)
+                                ),
+                        new SubcommandData("delete", "Delete a task")
+                                .addOptions(
+                                        new OptionData(OptionType.INTEGER, "team", "Team ID", true),
+                                        new OptionData(OptionType.INTEGER, "task", "Task ID", true)
+                                ),
+                        new SubcommandData("update", "Update a task’s title, description, or status.")
+                                .addOptions(
+                                        new OptionData(OptionType.INTEGER, "team", "Team ID", true),
+                                        new OptionData(
+                                                OptionType.INTEGER,
+                                                "id",
+                                                "ID of the task to update.",
+                                                true
+                                        ),
+                                        new OptionData(
+                                                OptionType.STRING,
+                                                "title",
+                                                "New title for the task.",
+                                                false
+                                        ).setRequiredLength(1, 32),
+                                        new OptionData(
+                                                OptionType.STRING,
+                                                "description",
+                                                "New description for the task.",
+                                                false
+                                        ).setRequiredLength(1, 256),
+                                        new OptionData(
+                                                OptionType.INTEGER,
+                                                "type",
+                                                "New status for the task.",
+                                                false
+                                        )
+                                                .addChoice(
+                                                        TaskStatus.COMPLETED.getIcon() + " " + TaskStatus.COMPLETED.getStringValue(),
+                                                        TaskStatus.COMPLETED.getIntValue())
+                                                .addChoice(
+                                                        TaskStatus.INPROGRESS.getIcon() + " " + TaskStatus.INPROGRESS.getStringValue(),
+                                                        TaskStatus.INPROGRESS.getIntValue())
+                                                .addChoice(
+                                                        TaskStatus.ARCHIVED.getIcon() + " " + TaskStatus.ARCHIVED.getStringValue(),
+                                                        TaskStatus.ARCHIVED.getIntValue())
+                                                .addChoice(
+                                                        TaskStatus.CANCELLED.getIcon() + " " + TaskStatus.CANCELLED.getStringValue(),
+                                                        TaskStatus.CANCELLED.getIntValue())
+                                ),
+                        new SubcommandData("list", "List all tasks")
+                                .addOptions(
+                                        new OptionData(OptionType.INTEGER, "team", "Team ID", true)
+                                ),
+
+                        new SubcommandData("assign", "Assign a task to a user")
+                                .addOptions(
+                                        new OptionData(OptionType.INTEGER, "team", "Team ID", true),
+                                        new OptionData(OptionType.INTEGER, "task", "Task ID", true),
+                                        new OptionData(OptionType.USER, "user", "User to assign", true)
+                                ),
+
+                        new SubcommandData("unassign", "Unassign a task from a user")
+                                .addOptions(
+                                        new OptionData(OptionType.INTEGER, "team", "Team ID", true),
+                                        new OptionData(OptionType.INTEGER, "task", "Task ID", true),
+                                        new OptionData(OptionType.USER, "user", "User to unassign", true)
+                                ),
+
+                        new SubcommandData("view", "View a task")
+                                .addOptions(
+                                        new OptionData(OptionType.INTEGER, "team", "Team ID", true),
+                                        new OptionData(OptionType.INTEGER, "task", "Task ID", true)
+                                ))
+        };
     }
 
     @Override
     public void execute(SlashCommandInteraction event) {
+        boolean subcommandGroup = event.getSubcommandGroup() != null;
+        String subCommand = event.getSubcommandName();
+        if (subCommand == null) {
+            event.reply("Invalid subcommand.").setEphemeral(true).queue();
+            return;
+        }
+
+        // Prevent timeout by acknowledging interaction
+        event.deferReply().queue();
+
+        MessageEmbed messageEmbed;
+
+        TeamSubCommandFunctions subCommandHandler = new TeamSubCommandFunctions(TEAM_SERVICE);
+
+        if (subcommandGroup) {
+            messageEmbed = switch (subCommand) {
+                case "create" -> subCommandHandler.create(event);
+//                case "delete" -> subCommandHandler.cancelTask(event);
+                case "update" -> subCommandHandler.delete(event);
+                case "list" -> subCommandHandler.readAll(event);
+                case "assign" -> subCommandHandler.update(event);
+                case "unassign" -> subCommandHandler.read(event);
+                case "view" -> subCommandHandler.read(event);
+                default -> null;
+            };
+        } else {
+            messageEmbed = switch (subCommand) {
+                case "create" -> subCommandHandler.create(event);
+//                case "add" -> subCommandHandler.cancelTask(event);
+                case "remove" -> subCommandHandler.delete(event);
+                case "accept" -> subCommandHandler.readAll(event);
+                case "reject" -> subCommandHandler.update(event);
+                case "leave" -> subCommandHandler.read(event);
+                case "view" -> subCommandHandler.read(event);
+                default -> null;
+            };
+        }
+
+        if (messageEmbed == null) {
+            event.getHook()
+                    .editOriginal("❌ An unexpected error occurred (embed was null).")
+                    .queue();
+            return;
+        }
+
+        // Send the rendered embed
+        event.getHook().editOriginalEmbeds(messageEmbed).queue();
 
     }
 }
