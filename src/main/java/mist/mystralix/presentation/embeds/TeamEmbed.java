7347a786dc950965531c5987a85b0b1f9347b2f0
softmist_1 <aljohnstevenmiclat.KES@gmail.com>
2025-12-07 18:08:25 -0600

    === Clock Work | 0.0.0.0.2.18.46 ===
    - TeamEmbed.java
    * Initiated MessageEmbed functions in TeamEmbed for teams-related messages

--- src/main/java/mist/mystralix/presentation/embeds/TeamEmbed.java
+++ src/main/java/mist/mystralix/presentation/embeds/TeamEmbed.java
@@ -1,14 +1,30 @@
 package mist.mystralix.presentation.embeds;
 
+import mist.mystralix.domain.team.Team;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class TeamEmbed implements IMessageEmbedBuilder {
     @Override
     public <T> MessageEmbed createMessageEmbed(User user, String title, T object) {
-        return null;
+        if (!(object instanceof Team team)) return null;
+
+        EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle(title + " | Team #" + team.getId());
+        embedBuilder.setColor(Color.GREEN);
+        embedBuilder.setDescription(
+                "Name: " + team.getTeamName() + "\n"
+        );
+        embedBuilder.setFooter(
+                user.getEffectiveName() + " | Team",
+                user.getEffectiveAvatarUrl()
+        );
+
+        return embedBuilder.build();
     }
 
     @Override
