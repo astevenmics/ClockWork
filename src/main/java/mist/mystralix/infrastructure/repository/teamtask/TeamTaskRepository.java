199058c3fb5b904323bff1756be65ed4f6b8be98
ItsMystic01 <aljohnstevenmiclat.kes@gmail.com>
2025-12-19 21:59:11 -0600

    === Clock Work | 0.0.0.0.2.18.71.5 ===
    - TeamTaskRepository.java
    * Removed unused imports

--- src/main/java/mist/mystralix/infrastructure/repository/teamtask/TeamTaskRepository.java
+++ src/main/java/mist/mystralix/infrastructure/repository/teamtask/TeamTaskRepository.java
@@ -1,7 +1,6 @@
 package mist.mystralix.infrastructure.repository.teamtask;
 
 import mist.mystralix.domain.task.TeamTask;
-import mist.mystralix.domain.team.Team;
 import mist.mystralix.infrastructure.repository.base.BaseRepository;
 
 public interface TeamTaskRepository extends BaseRepository<TeamTask> { }
